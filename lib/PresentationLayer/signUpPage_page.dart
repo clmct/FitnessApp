52865d647caa1c710d7d651e2bac7a348b4c9f62
Almat Kulbaev <almatkulbaev@Almats-MacBook-Pro.local>
2021-07-06 13:38:26 +0700

    Add Auth Widget

---
+++ lib/PresentationLayer/signUpPage_page.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import 'package:fitness_app/PresentationLayer/main.dart';
+
+// class SignUpPagePage extends StatefulWidget {
+//   static const String routeName = '/signUpPage';
+
+//   @override
+//   _SignUpPagePageState createState() => _SignUpPagePageState();
+// }
+
+// class _SignUpPagePageState extends State<SignUpPagePage> {
+//   final _signUpPageBloc = SignUpPageBloc();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SignUpPage'),
+//       ),
+//       body: SignUpPageScreen(signUpPageBloc: _signUpPageBloc),
+//     );
+//   }
+// }
